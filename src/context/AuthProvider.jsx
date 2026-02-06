d1720260063f5dd4b92cc89a483d5b3becac5f57
subhashish005 <subhashishsamant001@gmail.com>
2026-02-06 12:33:15 +0530

    Update AuthProvider to automatically login user if possible

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,9 +1,34 @@
-import { createContext, useState } from "react";
+import { createContext, useLayoutEffect, useState } from "react";
+import { axiosPrivate } from "../utils/getAxiosInstance";
+import { jwtDecode } from "jwt-decode";
 
 export const AuthContext = createContext({});
 
 export const AuthProvider = ({children}) => {
   const [auth, setAuth] = useState({isLoggedIn: false});
+
+  useLayoutEffect(() => {
+    const init = async () => {
+      await axiosPrivate.get('/api/v1/auth/renew-access-token')
+        .then((response) => {
+
+          const token = response.data?.access_token;
+          const username = jwtDecode(token).sub;
+
+          setAuth({
+            accessToken: token,
+            isLoggedIn: true,
+            username
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+    }
+
+    init();
+
+  }, []);
 
   return (
     <AuthContext.Provider value={{auth, setAuth}}>
--- src/utils/getAxiosInstance.js
+++ src/utils/getAxiosInstance.js
@@ -4,6 +4,7 @@ const BASE_URL = "http://localhost:8080";
 
 export const axiosPublic = axios.create({
   baseURL: BASE_URL,
+  withCredentials: true,
 });
 
 export const axiosPrivate = axios.create({
